8c365c40c6731130ba7d4417402c0de7fc292380
Himanshu Bhange <himanshubhange886@gmail.com>
2024-05-28 08:56:30 +0530

    Changes made on branch B-2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,13 @@
 class HelloWorld{
+
+    static void printName(String name) {
+        System.out.println("Hello there " + name);
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        System.out.println("Some features added by B-2");
+        printName("Himanshu");
     }
 }
